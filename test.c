8fdeaf70e6b711cafc8ec40b6773c4b194a3757b
Mian Hassan Aadil <120937223+Mianhassan-Aadil@users.noreply.github.com>
2022-12-25 20:18:14 +0100

    Add files via upload

---
+++ test.c
@@ -0,0 +1,23 @@
+#include<stdio.h>  
+#include<conio.h>
+
+int main(){
+    int a,b;
+    printf("Enter two numbers: ");
+    scanf("%d%d",&a,&b);
+    printf("You Entered %d and %d", a,b);
+    fun(&a,&b);
+    printf("\nFlipped numbers are %d and %d", a,b);
+
+    return 0;
+}
+
+int fun(int *p1, int *p2){
+    int c,d;
+    c = *p1;
+    d = *p2;
+
+    *p1 = d;
+    *p2 = c;
+}
+
